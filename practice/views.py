282e741a70012371f7ffb9069b78fd1f2efe0eb7
William Chau <w.chau1@student.han.nl>
2024-12-14 00:07:22 +0100

    Ex. 17 Adding Template Context

--- practice/views.py
+++ practice/views.py
@@ -3,10 +3,26 @@ from django.views.generic import TemplateView
 class FilterView(TemplateView):
     template_name = 'practice/filters.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = 'Webucator'
+        context['url'] = 'https://www.webucator.com'   
+        return context    
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class TagView(TemplateView):
     template_name = 'practice/tags.html'
-    
-        
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['beatles'] = [
+            {'firstname': 'Paul', 'lastname': 'McCartney'},
+            {'firstname': 'John', 'lastname': 'Lennon'},
+            {'firstname': 'George', 'lastname': 'Harrison'},
+            {'firstname': 'Ringo', 'lastname': 'Starr'},
+        ]    
+        return context
+
+     
